96ec28b5f3853cb41acdece1e8bcace6bdb17f31
Johan_Trieloff <johan@trieloff.net>
2023-12-31 16:07:47 +0100

    second change

--- blocks/test.js
+++ blocks/test.js
@@ -8,5 +8,5 @@ function createdWithMergeAndNotChangedAfter() {
 function CreatedOnMainAndNotChangedAfterMerge() {
     console.log('This creates the function on main branch')
     console.log('First change on test branch')
-    
+    console.log('Second change on test branch')
 }
